e5d46c3f9941b23a10e4409d7d2608cb596594df
Davide Raro <davide.raro@me.com>
2025-04-09 12:30:43 +0300

    update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
